01f2d2eafaeb3b12062d727af97d7c951c79988e
lenameister <lena.meister27@gmail.com>
2022-12-20 14:53:15 +0100

    todo liste

--- Aufgabe10/typescript.js
+++ Aufgabe10/typescript.js
@@ -1,10 +1,9 @@
 var A10;
-(function (A10) {
+
     let counter = 0;
     let input = document.querySelector("task");
     function newTask() {
         counter++;
         document.querySelector("todocounter").innerHTML = String(counter + " in total");
     }
-})(A10 || (A10 = {}));
 //# sourceMappingURL=typescript.js.map
