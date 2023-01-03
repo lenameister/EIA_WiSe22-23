a9cf077e47b8dfeb2a8f76c041e4d0cf0a45fafa
lenameister <lena.meister27@gmail.com>
2023-01-03 17:01:04 +0100

    todo liste

--- Aufgabe10/typescript.js
+++ Aufgabe10/typescript.js
@@ -1,9 +1,50 @@
 var A10;
-
-    let counter = 0;
-    let input = document.querySelector("task");
-    function newTask() {
-        counter++;
-        document.querySelector("todocounter").innerHTML = String(counter + " in total");
-    }
+(function (A10) {
+    window.addEventListener("load", function () {
+        let counter = 0;
+        let input = document.querySelector("#textfield");
+        document.querySelector("#addtn").addEventListener("click", newTask);
+        function newTask() {
+            counter++;
+            document.querySelector("todocounter").innerHTML = String(counter + " in total");
+            let eingabefeld = document.createElement("p");
+            eingabefeld.innerHTML = input.value;
+            input.value = "";
+            let task = document.getElementById("task");
+            let check = document.createElement("div");
+            let trash = document.createElement("div");
+            let wrapper = document.createElement("div");
+            wrapper.textContent = "";
+            task.appendChild(wrapper);
+            wrapper.appendChild(eingabefeld);
+            wrapper?.appendChild(check);
+            wrapper?.appendChild(trash);
+            task?.appendChild(wrapper);
+            eingabefeld.id = "eingabe";
+            wrapper.id = "neuesDiv";
+            check.className = "far fa-circle";
+            trash.className = "fas fa-trash";
+            check.addEventListener("click", erledigteToDos);
+            function erledigteToDos() {
+                if (check.getAttribute("class") == "far fa-check-circle") {
+                    check.setAttribute("class", "far fa-circle");
+                }
+                else {
+                    check.setAttribute("class", "far fa-check-circle");
+                }
+            }
+            trash.addEventListener("click", deleteToDo);
+            function deleteToDo() {
+                wrapper.parentElement.removeChild(wrapper);
+                counter--;
+                document.querySelector("#todoAmount").innerHTML = String(counter + " in total");
+            }
+        }
+        document.addEventListener("keydown", function (event) {
+            if (event.key == "Enter") {
+                newTask();
+            }
+        });
+    });
+})(A10 || (A10 = {}));
 //# sourceMappingURL=typescript.js.map
--- Aufgabe10/typescript.ts
+++ Aufgabe10/typescript.ts
@@ -1,12 +1,63 @@
 namespace A10 {
-    let counter = 0;
-    let input = document.querySelector("task");
+    window.addEventListener("load", function (): void {
+        let counter = 0;
+        let input: HTMLInputElement = document.querySelector("#textfield");
+        document.querySelector("#addtn").addEventListener("click", newTask);
 
-    function newTask() {
-        counter++;
-        document.querySelector("todocounter").innerHTML = String(counter + " in total");
 
-        
-    }
+        function newTask() {
+            counter++;
+            document.querySelector("todocounter").innerHTML = String(counter + " in total");
 
+            let eingabefeld: HTMLElement = document.createElement("p");
+            eingabefeld.innerHTML = input.value;
+
+            input.value = "";
+
+            let task: HTMLElement = document.getElementById("task");
+            let check: HTMLDivElement = document.createElement("div");
+            let trash: HTMLDivElement = document.createElement("div");
+            let wrapper: HTMLDivElement = document.createElement("div");
+
+            wrapper.textContent = "";
+
+            task.appendChild(wrapper);
+            wrapper.appendChild(eingabefeld);
+            wrapper?.appendChild(check);
+            wrapper?.appendChild(trash);
+            task?.appendChild(wrapper);
+
+            eingabefeld.id = "eingabe";
+            wrapper.id = "neuesDiv";
+            check.className = "far fa-circle";
+            trash.className = "fas fa-trash";
+
+
+            check.addEventListener("click", erledigteToDos);
+
+            function erledigteToDos(): void {
+                if (check.getAttribute("class") == "far fa-check-circle") {
+                    check.setAttribute("class", "far fa-circle");
+                }
+                else {
+                    check.setAttribute("class", "far fa-check-circle");
+                }
+            }
+
+            trash.addEventListener("click", deleteToDo);
+
+            function deleteToDo(): void {
+                wrapper.parentElement.removeChild(wrapper);
+                counter--;
+                document.querySelector("#todoAmount").innerHTML = String(counter + " in total");
+            }
+
+        }
+
+        document.addEventListener("keydown", function (event: KeyboardEvent): void {
+            if (event.key == "Enter") {
+                newTask();
+            }
+        });
+    });
 }
